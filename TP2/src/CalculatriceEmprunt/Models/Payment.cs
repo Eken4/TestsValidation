c08dee857d420477adf0dcc79844af94876a00c4
Eken4 <juliette.bressy@free.fr>
2024-03-26 23:38:59 +0100

    tp2

---
+++ TP2/src/CalculatriceEmprunt/Models/Payment.cs
@@ -0,0 +1,21 @@
+﻿namespace CalculatriceEmprunt.Models
+{
+    public class Payment
+    {
+        public int Id { get; set; }
+        public double AmountLeft { get; set; }
+        public double AmountPaid { get; set; }
+
+        public Payment(int id, double amountPaid, double amountLeft)
+        {
+            Id = id;
+            AmountPaid = amountPaid;
+            AmountLeft = amountLeft;
+        }
+
+        public string ToString()
+        {
+            return string.Join(",", Id, AmountPaid, AmountLeft);
+        }
+    }
+}
---
+++ TP2/src/CalculatriceEmpruntTest/PaymentTest.cs
@@ -0,0 +1,19 @@
+﻿using CalculatriceEmprunt.Models;
+
+namespace CalculatriceEmpruntTest
+{
+    public class PaymentTest
+    {
+        public PaymentTest() { }
+
+        [Theory]
+        [InlineData(1, 5000, 15000, "1,5000,15000")]
+        public void ToStringTest(int id, double amountPaid, double amountLeft, string expectedOutput)
+        {
+            Payment payment = new Payment(id, amountPaid, amountLeft);
+            Assert.Equal(expectedOutput, payment.ToString());
+
+        }
+
+    }
+}
